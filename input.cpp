a70aa3f93d62855b6dfe050b6c42e8589c93338a
Duc <48994241+nduc03@users.noreply.github.com>
2022-06-06 23:53:03 +0700

    Rename project, refactor

--- input.cpp
+++ input.cpp
@@ -4,21 +4,7 @@
 #include <conio.h>
 #include <Windows.h>
 #include "Matrix.h"
-
-const int SPECIAL_KEY_1 = 0;
-const int SPECIAL_KEY_2 = 224;
-const int ARROW_UP = 72;
-const int ARROW_LEFT = 75;
-const int ARROW_RIGHT = 77;
-const int ARROW_DOWN = 80;
-const int CHAR_SLASH = 47;
-const int CHAR_MINUS = 45;
-const int CHAR_NINE = 57;
-const int CHAR_ZERO = 48;
-const int KEY_ENTER = 13;
-const int INVALID_KEY = -1;
-const int BACKSPACE = 8;
-const int CHAR_POINT = 46;
+#include "nduc/Keycode.h"
 
 std::string trim(const std::string& value) {
 	std::string result = value;
